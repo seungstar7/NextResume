e4026de8107a5cb87a3dd5e8602eebef5c464168
shkim <shkim0922@naver.com>
2025-01-08 19:24:22 +0900

    init seung's Resume

---
+++ payload/education.ts
@@ -0,0 +1,22 @@
+import { IEducation } from '../component/education/IEducation';
+
+const education: IEducation.Payload = {
+  disable: false,
+
+  list: [
+    {
+      title: 'University of Open Source',
+      subTitle: 'PhD in System Engineering (Honorary)',
+      startedAt: '1991-03',
+      // endedAt: '2010-02',
+    },
+    {
+      title: 'Antarctica High School',
+      subTitle: 'Graduated',
+      startedAt: '1989-03',
+      endedAt: '1991-02',
+    },
+  ],
+};
+
+export default education;
---
+++ payload/profile.ts
@@ -0,0 +1,44 @@
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
+
+import { faBell } from '@fortawesome/free-regular-svg-icons';
+import { IProfile } from '../component/profile/IProfile';
+import image from '../asset/sample_tux.png';
+
+const profile: IProfile.Payload = {
+  disable: false,
+
+  // image: 'https://resume.yowu.dev/static/image/profile_2019.png',
+  image,
+  name: {
+    title: 'Tux Linux Penguin',
+    small: '(Tux)',
+  },
+  contact: [
+    {
+      title: 'tux@linux.org',
+      link: 'mailto:tux@linux.org',
+      icon: faEnvelope,
+    },
+    {
+      title: 'Please contact me by email',
+      icon: faPhone,
+      badge: true,
+    },
+    {
+      link: 'https://github.com/uyu423/resume-nextjs',
+      icon: faGithub,
+    },
+    {
+      link: 'https://www.facebook.com/groups/ubuntu.ko',
+      icon: faFacebook,
+    },
+  ],
+  notice: {
+    title:
+      "The content below is all fictitious and is just a sample from 'https://github.com/uyu423/resume-nextjs'.",
+    icon: faBell,
+  },
+};
+
+export default profile;
