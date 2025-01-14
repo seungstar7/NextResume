ec57924060e5c872941aee38a1c932d95554ac9f
KIM <shkim0922@naver.com>
2025-01-14 10:46:28 +0900

    education, 프로젝트 경력 업데이트

--- payload/education.ts
+++ payload/education.ts
@@ -5,16 +5,22 @@ const education: IEducation.Payload = {
 
   list: [
     {
-      title: '가톨릭대학교',
-      subTitle: '컴퓨터공학 / 정보시스템공학 복수 전공 학사 졸업',
-      startedAt: '2010-03',
-      endedAt: '2016-08',
+      title: '한양사이버대학교',
+      subTitle: '컴퓨터공학 학사 졸업',
+      startedAt: '2023-03',
+      endedAt: '2025-02',
     },
     {
-      title: '안동 경일고등학교',
-      subTitle: '자연계 졸업 (경상북도 안동시)',
-      startedAt: '2007-03',
-      endedAt: '2010-02',
+      title: '명지전문대학',
+      subTitle: '음악 전문 학사 졸업',
+      startedAt: '2012-03',
+      endedAt: '2014-02',
+    },
+    {
+      title: '서울공연예술고등학교',
+      subTitle: '음악과 졸업 (실용음악)',
+      startedAt: '2009-03',
+      endedAt: '2012-02',
     },
   ],
 };
